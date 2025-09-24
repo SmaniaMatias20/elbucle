805e7cfb808bc6fe956a620eab074b3cb1d3e4e8
Matias <matiasezequielsmania@gmail.com>
2025-09-24 16:05:57 -0300

    .

--- index.js
+++ index.js
@@ -65,6 +65,7 @@ app.post('/send-confirmation-mail', async (req, res) => {
 });
 
 
-app.listen(3000, () => {
-    console.log('Servidor corriendo en http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
