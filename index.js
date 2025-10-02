463666f19f1b0a258ff838858374b8c66f843a0c
Matias <matiasezequielsmania@gmail.com>
2025-10-01 23:24:32 -0300

    .

--- index.js
+++ index.js
@@ -1,53 +1,4 @@
-// import express from 'express';
-// import cors from 'cors';
-// import bodyParser from 'body-parser';
-// import admin from 'firebase-admin';
-// import { createClient } from '@supabase/supabase-js';
-// import { notifyUserStatus } from './mailer.js';
-// import fs from 'fs';
-// const admin = require('firebase-admin');
 
-// // Inicializar Firebase Admin
-// // const serviceAccount = JSON.parse(fs.readFileSync('./serviceAccountKey.json'));
-// // admin.initializeApp({
-// //     credential: admin.credential.cert(serviceAccount)
-// // });
-
-// // Inicializar Supabase
-// // const supabase = createClient('https://dgnvjlzhaoxhaftpdurq.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRnbnZqbHpoYW94aGFmdHBkdXJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ1NTUxNTEsImV4cCI6MjA2MDEzMTE1MX0.yjRBf1UdsEqWm8YBbB7NSXYtVqgLV_J65TTDvR_DWsQ');
-
-// const app = express();
-// app.use(cors());
-// app.use(bodyParser.json());
-
-
-// // Carga el archivo JSON de configuración que descargaste desde Firebase
-// const serviceAccount = require('./serviceAccountKey.json'); // Reemplaza con la ruta de tu archivo
-
-// // Inicializa Firebase Admin
-// admin.initializeApp({
-//     credential: admin.credential.cert(serviceAccount),
-// });
-
-// // Endpoint para enviar el mail de confirmación de registro
-// app.post('/send-confirmation-mail', async (req, res) => {
-//     console.log(req.body);
-//     const { name, email, statusString } = req.body;
-//     if (!name || !email) return res.status(400).send('Faltan parámetros');
-
-//     const user = { name, email };
-
-//     // Enviar correo de confirmación
-//     notifyUserStatus(user, statusString);
-
-//     res.send({ success: true, message: 'Correo enviado' });
-// });
-
-
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//     console.log(`Servidor corriendo en http://localhost:${PORT}`);
-// });
 
 import express from 'express';
 import cors from 'cors';
@@ -57,11 +8,18 @@ import { createClient } from '@supabase/supabase-js';
 import { notifyUserStatus } from './mailer.js';
 import fs from 'fs';
 import dotenv from 'dotenv';
-// Si estás utilizando ESModules, deberías usar 'import'. 
-// Si no, asegúrate de que tu 'package.json' tenga "type": "module".
+import cors from 'cors';
+
+const corsOptions = {
+    origin: ['capacitor://localhost', 'http://localhost', 'http://localhost:8100'], // Orígenes permitidos
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true, // si usas cookies o auth con credenciales
+};
+
+app.use(cors(corsOptions));
 
 const app = express();
-app.use(cors());
 app.use(bodyParser.json());
 dotenv.config();
 
