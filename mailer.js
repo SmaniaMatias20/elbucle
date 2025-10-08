838c7922efc84529c7ea1c3554f814ae180c4143
Matias <matiasezequielsmania@gmail.com>
2025-10-08 13:02:47 -0300

    .

--- mailer.js
+++ mailer.js
@@ -4,8 +4,8 @@ require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
+    port: 587,
+    secure: false, // STARTTLS
     auth: {
         user: process.env.EMAIL,
         pass: process.env.PASS,
