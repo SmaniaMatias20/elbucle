83c285e84dc7001875716f991588a0c5fa0e6f34
Matias <matiasezequielsmania@gmail.com>
2025-10-27 11:41:41 -0300

    .

--- index.js
+++ index.js
@@ -31,6 +31,10 @@ app.use(bodyParser.json());
 
 app.use('/public', express.static('public'));
 
+// 🔹 Aumentar el límite máximo permitido a 50 MB
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
 admin.initializeApp({
     credential: admin.credential.cert({
         projectId: process.env.GOOGLE_PROJECT_ID,
